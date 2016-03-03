69b159904f244d0c408e2b923826e882b772793d
Les Green <lgreen@cmedtechnology.com>
2016-03-03 15:36:55 +0000

    attempt to add autofill passwords

--- defaults.sh
+++ defaults.sh
@@ -4,3 +4,5 @@
 defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool true
 
+# Attempt to stop autofill passwords from showing up
+defaults write com.apple.Safari AutoFillPasswords -bool false
